c077cbbd105ad6e7453f93113bd31c59bb66cb45
ps219322 <74542745+ps219322@users.noreply.github.com>
2022-06-21 15:31:37 +0200

    login scherm

--- SummaMoveAPP/components/Login.js
+++ SummaMoveAPP/components/Login.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 function App({ navigation }) {
     return (
     <View style={styles.container}>
-      <Text>Meld je aan om je prestaties te kunnen bekijken</Text>
+      <Text style={styles.tekst}>Meld je aan om je prestaties te kunnen bekijken{"\n"}</Text>
+      <TextInput style={styles.input} placeholder="Gebruikersnaam"></TextInput>
+      <TextInput style={styles.input} placeholder="Wachtwoord"></TextInput>
+      <Text>{"\n"}</Text>
       <StatusBar style="auto" />
-      <Button onPress={() => navigation.navigate('LoadPrestaties')} title="Login"/>
+      <Button style={ styles.login } onPress={() => navigation.navigate('LoadPrestaties')} title="Login"/>
     </View>
   );
 }
@@ -15,9 +18,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+    width: 300,
+    alignSelf: 'center',
+  },
+  tekst: {
+    alignSelf: 'center',
+    fontSize: 15,
+    fontWeight: 'bold',
+
+  }
 });
 
 export default App;
