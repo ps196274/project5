9f17103564ace4bb71030127a2a1c3b62b21f5d0
ps219322 <74542745+ps219322@users.noreply.github.com>
2022-06-30 10:47:46 +0200

    App api inladen werkt

--- SummaMoveAPP/App.js
+++ SummaMoveAPP/App.js
@@ -90,7 +90,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
 });
 
--- SummaMoveAPP/components/Oefeningen.js
+++ SummaMoveAPP/components/Oefeningen.js
@@ -1,14 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, FlatList, Text, View } from 'react-native';
 
-function App() {
+const App = ({ navigation }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getApi = async () => {
+     try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.titel}>Bekijk hier alle oefeningen</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.titel}>{"\n"}  Een lijst met oefeningen{"\n"}  {"\n"}  </Text>
+      {isLoading ? <ActivityIndicator/> : (
+      <FlatList
+      data={data}
+      keyExtractor={({ id }, index) => id}
+      renderItem={({ item }) => <Text>
+      {item.name}{"\n"}        
+      {item.email}{"\n"} </Text>
+      }
+    />
+      )}
+    </View>    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -22,5 +51,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   }
 });
-
+ 
 export default App;
