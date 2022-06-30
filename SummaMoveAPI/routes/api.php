673bd696de110111736d243824562af53d167a2d
ps196274 <ps196274@edu.summacollege.nl>
2022-06-30 11:41:38 +0200

    login

--- SummaMoveAPI/routes/api.php
+++ SummaMoveAPI/routes/api.php
@@ -2,10 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\OefeningenController;
+use App\Http\Controllers\PrestatiesController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('oefeningen', OefeningenController::class)->parameters(['oefeningen' => 'oefening']);
+
+
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('profile', function(Request $request) {
+        return auth()->user();
+    });
+//    Route::apiResource('drivers', DriverController::class);
+    Route::apiResource('oefeningen', OefeningenController::class)->parameters(['oefeningen' => 'oefening']);
+//    Route::apiResource('constructors', ConstructorController::class)->parameters(['constructors' => 'constructor'])->only(['index', 'show']);
+
+//    Route::get('constructors/{id}/drivers', [DriverController::class, 'indexFunctie']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+
+});
+Route::fallback(function(){
+    return response()->json([
+        'message' => 'Page Not Found'], 404);
+});
+
