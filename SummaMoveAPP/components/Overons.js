9d80a6e24a3d7c9490c7c8ad11d16d38ea05a938
ps196274 <ps196274@edu.summacollege.nl>
2022-07-01 12:26:15 +0200

    Update Overons.js

--- SummaMoveAPP/components/Overons.js
+++ SummaMoveAPP/components/Overons.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 function App() {
   return (
     <View style={styles.container}>
-      <Text>Hallo wij zijn SummaMove</Text>
+      <Text style={styles.titleText}>Over ons</Text>
+      <Text>Deze app is gemaakt en ontwikkeld door studenten van de Summa College.
+        Deze app gaat er voor zorgen dat er studen van het Summa College meer gaan bewegen in plaats van achter de laptop zitten.
+        Op Summa Move staan allerlei soorten oefeningen en verschillende. De oefeningen vindt je hier onderin op de navigatiebalk bij "oefeningen".
+      </Text>
+      <Text style={styles.help}>Voor help klik op de knop rechtsboven "help"</Text>
+      <Text style={styles.versionnumber}>Versie: V1.0.0</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +22,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  help: {
+    fontSize: 15,
+    fontWeight: "italic"
+  },
+  versionnumber: {
+    fontSize: 10,
+    fontWeight: "italic"
   },
 });
 
